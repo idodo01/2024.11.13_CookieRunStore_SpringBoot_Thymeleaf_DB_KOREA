ac77a2fc4d08ac8fc79868c5d2164ebfca8e39a0
IDO <susususu07@naver.com>
2024-11-26 22:04:50 +0900

    Add files via upload

---
+++ Cookie Run Store (2)/src/main/resources/static/js/main/home.js
@@ -0,0 +1,6 @@
+const emblaNode = document.querySelector('.embla')
+const options = { loop: true }
+const plugins = [EmblaCarouselAutoplay()]
+const emblaApi = EmblaCarousel(emblaNode, options, plugins);
+console.log(emblaApi.slideNodes()) // Access API
+
