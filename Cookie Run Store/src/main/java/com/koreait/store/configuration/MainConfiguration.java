56c8d3b159d646147d7aceef4c9cdcb7dc921aae
IDO <susususu07@naver.com>
2024-12-02 21:29:49 +0900

    Delete Cookie Run Store directory

--- Cookie Run Store/src/main/java/com/koreait/store/configuration/MainConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.koreait.store.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@Configuration
-@EnableAspectJAutoProxy
-public class MainConfiguration {
-}
