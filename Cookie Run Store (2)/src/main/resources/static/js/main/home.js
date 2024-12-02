abf70a02b70077909f9449082991fad2e7b8860f
IDO <susususu07@naver.com>
2024-12-02 21:29:57 +0900

    Delete Cookie Run Store (2) directory

--- Cookie Run Store (2)/src/main/resources/static/js/main/home.js
+++
@@ -1,6 +0,0 @@
-const emblaNode = document.querySelector('.embla')
-const options = { loop: true }
-const plugins = [EmblaCarouselAutoplay()]
-const emblaApi = EmblaCarousel(emblaNode, options, plugins);
-console.log(emblaApi.slideNodes()) // Access API
-
